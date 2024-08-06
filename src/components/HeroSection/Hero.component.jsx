ed15debee7fb1528b69d94648fd41c68094c7416
Konduru Pranav Suhas Reddy <pranavsuhasreddykonduru@gmail.com>
2024-08-06 20:36:58 +0530

    hero componenet for phot editor --pranav

--- src/components/HeroSection/Hero.component.jsx
+++ src/components/HeroSection/Hero.component.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function HeroComponent() {
-  return <div>HeroComponent</div>;
+  return <div>
+    <div className="card">
+      <div className="image-output">
+        <p>img</p>
+      </div>
+      <div className="card-input">
+        <button><input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" />Add Image</button>
+      </div>
+    </div>
+  </div>;
 }
 
 export default HeroComponent;
