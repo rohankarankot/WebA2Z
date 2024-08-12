d16fe1d756ea393b3de058af594059e1fcc8549c
Rohan Karankot <rohankarankot@gmail.com>
2024-08-12 21:29:35 +0530

    pdp start --exploiter

--- src/components/itemCard/itemCard.component.jsx
+++ src/components/itemCard/itemCard.component.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function ItemCardComponent({ product }) {
+  console.log("product: ", product);
   return (
-    <div className="card">
-      <h3>{product.vendor}</h3>
-      <img
-        src={product.thumbnail}
-        className="card-img-top"
-        alt={product.title}
-      />
-      <div className="card-body">
-        <h5 className="text-center">{product.title}</h5>
-        <p className="text-center">{product.des}</p>
-        <Link to={"/product"}>
-          <button className="btn btn-link"> View More</button>
-        </Link>
+    <Link to={`/product?id=${product.id}`} style={{ textDecoration: "none" }}>
+      <div className="card">
+        <h3>{product.vendor}</h3>
+        <img
+          src={product.thumbnail}
+          className="card-img-top"
+          alt={product.title}
+        />
+        <div className="card-body">
+          <h5 className="text-center">{product.title}</h5>
+          <p className="text-center">{product.des}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/components/pdp/index.jsx
+++ src/components/pdp/index.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 function ProductDec() {
-  return <div>this is the product desription</div>;
+  return (
+    <div className="container ">
+      <div className="my-4">
+        <div className="row">
+          <img
+            src="https://cookingfromheart.com/wp-content/uploads/2017/03/Paneer-Tikka-Masala-4.jpg"
+            alt=""
+            style={{ width: "500px", objectFit: "contain" }}
+            className="col-md-6"
+          />
+
+          <div className="pdp col-md-6">
+            <p style={{ fontSize: "24px" }}>Product's title</p>
+            askldfklsdf Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellat dolores enim delectus pariatur error, sapiente quam eaque
+            culpa nobis ipsam rerum eius aspernatur non fugit laboriosam, rem
+            cum magni aliquam! Repellat eligendi repudiandae velit dolorem
+            adipisci. Assumenda qui nulla debitis nisi, voluptas rem
+            necessitatibus earum veritatis, ut natus expedita quibusdam quam
+            officiis tempora doloremque repellendus deleniti ullam quos repellat
+            sit laudantium cumque, perspiciatis molestiae. Totam vel deleniti
+            labore officia, quisquam illum quis quos inventore tempore minus
+            eius autem numquam eligendi delectus repellendus assumenda impedit
+            praesentium. Non recusandae exercitationem debitis commodi possimus
+            in soluta quis, laudantium nisi, doloribus placeat eum odio ratione
+            quaerat illum facilis dolor dicta voluptatem repellendus iusto?
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductDec;
