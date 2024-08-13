5fdc3ef715e4ff6902e44f9bc3779e4fbb656a1e
Konduru Pranav Suhas Reddy <pranavsuhasreddykonduru@gmail.com>
2024-08-13 21:43:31 +0530

    changed item card component --pranav

--- src/components/itemCard/itemCard.component.jsx
+++ src/components/itemCard/itemCard.component.jsx
@@ -2,22 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function ItemCardComponent({ product }) {
-  console.log("product: ", product);
+  let ratingc;
+  if(product.rating>4)
+    ratingc= "rgb(87,227,44)";
+  else if(product.rating>3&&product.rating<4)
+    ratingc="rgb(255,165,52)";
+  else
+  ratingc="rgb(255,69,69)"
   return (
-    <Link to={`/product?id=${product.id}`} style={{ textDecoration: "none" }}>
-      <div className="card">
-        <h3>{product.vendor}</h3>
-        <img
-          src={product.thumbnail}
-          className="card-img-top"
-          alt={product.title}
-        />
-        <div className="card-body">
-          <h5 className="text-center">{product.title}</h5>
-          <p className="text-center">{product.des}</p>
+    <div className="card">
+      <h3>{product.vendor}</h3>
+      <img
+        src={product.thumbnail}
+        className="card-img-top"
+        alt={product.title}
+      />
+      <div className="card-body">
+        <h4 className="text-center">{product.title}</h4>
+        <div className="price">
+          <div className="price-before-discount" style={{display:'flex', gap: '10px'}}>
+            <b><i><p><s>₹{product.price}</s></p></i></b>
+            <i className="offer"><p>{product.discount}% off</p></i>
+          </div>
+          <h5>₹{product.price-product.price*product.discount/100}</h5>
         </div>
+        <h6>Rating: <span style={{color: ratingc}}>{product.rating}</span></h6>
+        <p className="text-center">{product.des}</p>
+        <Link to={"/product"}>
+          <button className="btn btn-link">View More</button>
+        </Link>
       </div>
-    </Link>
+    </div>
   );
 }
 
