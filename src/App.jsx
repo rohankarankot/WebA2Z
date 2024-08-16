dd89c1a8d0115632f367e09ff69a01d039f9769e
Rohan Karankot <rohankarankot@gmail.com>
2024-08-16 20:49:52 +0530

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,30 @@ import FooterComponent from "./components/footer/footer.component";
 import RootRouting from "./Routing";
 
 function App() {
+  const input = "a1b10";
+  let curr;
+  let res = [];
+
+  let input1 = input.split("");
+  input1?.map((element, index) => {
+    curr = element;
+    if (!!Number(element)) {
+      console.log(
+        "!!Number(element) && !!Number(input1[index + 1]): ",
+        !!Number(element) && !!Number(input1[index + 1])
+      );
+      if (!!Number(element) && !!Number(input1[index + 1])) {
+        console.log(`${element}${input1[index + 1]}`);
+      }
+    } else console.log("element: ", element);
+  });
+
   return (
     <>
-      <NavbarComponent />
+      {/* <NavbarComponent />
       <RootRouting />
-      <FooterComponent />
+      <FooterComponent /> */}
+      hello
     </>
   );
 }
