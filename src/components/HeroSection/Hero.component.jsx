e85f81ea1c1eb61d2bc607c29db6cf653466c4c2
sansrp358 <sanskrutirpaitwar@gmail.com>
2024-08-25 17:00:55 +0530

    heropage updated --sans

--- src/components/HeroSection/Hero.component.jsx
+++ src/components/HeroSection/Hero.component.jsx
@@ -6,16 +6,37 @@ function HeroComponent() {
       <div className="row pb-5">
         <div className="col-6 d-flex flex-column align-items-start px-lg-5 heading-style mt-3">
           <p>
-            Fastest
-            <span style={{ color: "coral" }}>Delivery</span> <br /> & Easy
-            <span style={{ color: "coral" }}>Pickup</span>
+            <span style={{ color: "black" }}>Fastest </span>
+            <span style={{ color: "coral" }}>Delivery</span> <br />
+            <span style={{ color: "coral" }}> & Easy </span>
+            <span style={{ color: "black" }}>Pickup</span>
           </p>
-          <div className="row">
+          <div className="chat-container">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZR6Gs7vDfFNNMsCAc2pNG0LaG3xAgnZDapQ&usqp=CAU"
               alt="miniprofile"
               style={{ width: "90px", marginTop: "10px" }}
+              className="chat-image"
             />
+            <div className="chat-message">
+              <p style={{ color: "black" }}>
+                When you are too lazy to cook, we are just a click away
+              </p>
+            </div>
+          </div>
+          <div className="container">
+            <div className="find-restaurant">
+              <button className="find-button">
+                <i className="fas fa-search"></i> <b>Find Restaurants</b>
+              </button>
+            </div>
+            <div className="how-to-order">
+              <i
+                className="fas fa-play playIcon"
+                style={{ color: "coral" }}
+              ></i>
+              <b style={{ color: "coral" }}>How to Order?</b>
+            </div>
           </div>
         </div>
         <div className="col-4 d-flex justify-content-center align-items-center">
@@ -27,9 +48,18 @@ function HeroComponent() {
         </div>
         <div className="col-2">
           <ul className="hero-ul">
-            <li>Fast Delivery</li>
-            <li>Pick Up</li>
-            <li>Dine In</li>
+            <li>
+              <i className="fas fa-truck-fast"></i>
+              <b style={{ fontSize: "19px" }}>Fast Delivery</b>
+            </li>
+            <li>
+              <i className="fas fa-box"></i>
+              <b style={{ fontSize: "19px" }}>Pick Up</b>
+            </li>
+            <li>
+              <i className="fas fa-utensils"></i>
+              <b style={{ fontSize: "19px" }}>Dine In</b>
+            </li>
           </ul>
         </div>
       </div>
